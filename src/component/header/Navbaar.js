02ee9456600528d0b58aa3244cd899cc564a6024
Sasikala <sashipemmanaboidi@gmail.com>
2022-06-28 20:36:04 +0530

    amazoncloneapp

---
+++ src/component/header/Navbaar.js
@@ -0,0 +1,203 @@
+import {React, useState, useContext, useEffect} from 'react';
+import "./navbaar.css";
+import SearchIcon from '@mui/icons-material/Search';
+import Badge from '@mui/material/Badge';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Avatar from '@mui/material/Avatar';
+import { NavLink, useNavigate } from "react-router-dom";
+import { LoginContext } from '../context/ContextProvider';
+import { IconButton,Drawer } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import Rightheader from './Rightheader';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import LogoutIcon from '@mui/icons-material/Logout';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import { useSelector } from 'react-redux';
+import {ToastContainer, toast} from 'react-toastify';
+
+const Navbaar = () => {
+
+    const {account, setAccount} = useContext(LoginContext);
+    console.log(account);
+
+    const history = useNavigate();
+
+    const [anchorE1, setAnchorE1] = useState(null);
+    const open = Boolean(anchorE1);
+    const handleClick = (event) =>{
+        setAnchorE1(event.currentTarget);
+    };
+    const handleClose = () =>{
+        setAnchorE1(null);
+    };
+    const [text, setText]= useState("");
+    console.log(text);
+
+    const [liopen, setLiopen] = useState(true);
+    
+    const {products} = useSelector(state => state.getproductsdata);
+
+    const [dropen, setDropen] = useState(false)
+    //console.log(account);
+    const getdetailvaliduser = async()=>{
+        const res = await fetch("/validuser",{
+            method:"GET",
+            headers:{
+                "Accept":"application/json",
+                "Content-Type":"application/json"
+            },
+            credentials:"include"
+        });
+        const data = await res.json();
+        //console.log(data);
+        if(res.status !== 201){
+             console.log("error");
+        }else{
+            console.log("data valid");
+            setAccount(data);
+       }
+    };
+
+    const handleopen = ()=>{
+        setDropen(true)
+    }
+     
+    const handledrclose = ()=>{
+        setDropen(false)
+    }
+
+    const logoutuser = async () =>{
+        const res2 = await fetch("/lougout",{
+            method:"GET",
+            headers:{
+                "Accept" :"application/json",
+                "Content-Type" :"application/json"
+            },
+            credentials:"include"
+        });
+
+        const data2 = await res2.json();
+        console.log(data2);
+
+        if(res2.status !== 201){
+            console.log("error");
+        }else{
+            console.log("data valid log");
+            //alert("logout");
+            toast.success("user logout", {
+                position: "top-center",
+            })
+            history("/")
+            setAccount(false);
+        }
+    };
+
+    const getText =(iteams) =>{
+        setText(iteams)
+        setLiopen(false)
+    }
+
+    useEffect(()=>{
+        getdetailvaliduser();
+
+    },[])
+
+  return (
+    <header>
+        <nav>
+            <div className='left'>
+                <IconButton className='hamburgur' onClick={handleopen}>
+                    <MenuIcon style={{color:"#fff"}}/>
+                </IconButton>
+                <Drawer open={dropen} onClick={handledrclose}>
+                    <Rightheader Logclose={handledrclose} logoutuser={logoutuser}/>
+                </Drawer>
+                <div className='navlogo'>
+                <NavLink to="/">
+                    <img src='./amazon_PNG25.png' alt=''/>
+                </NavLink>    
+                </div> 
+                <div className='nav_searchbaar'>
+                    <input type="text" name="" onChange={(e)=> getText(e.target.value)} placeholder='Search your product' id=""/>
+                    <div className='search_icon'>
+                        <SearchIcon id="search"/>
+                    </div>
+                    {
+                            text &&
+                            <List className='extrasearch' hidden={liopen}>
+                                {
+                                    products.filter(product => product.title.longTitle.toLowerCase().includes(text.toLowerCase())).map(product => (
+                                        <ListItem>
+                                            <NavLink to={`/getproductsone/${product.id}`} onClick={() => setLiopen(true)} >
+                                                { product.title.longTitle }
+                                        </NavLink>
+                                          
+                                        </ListItem>
+                                    ))
+                                }
+                    </List>
+                        }
+                </div>
+            </div>
+            <div className='right'>
+                <div className='nav_btn'>
+                    <NavLink to="/login">signin</NavLink>
+                </div>
+                <div className='cart_btn'>
+                    {
+                        account ?  <NavLink to="/buynow">
+                        <Badge badgeContent={account.carts.length} color="secondary">
+                           <ShoppingCartIcon id="icon"/>
+                        </Badge>
+                        </NavLink>:
+                        <NavLink to="/login">
+                        <Badge badgeContent={0} color="secondary">
+                           <ShoppingCartIcon id="icon"/>
+                        </Badge>
+                        </NavLink>
+                    }
+                    <ToastContainer/>
+                     <p>Cart</p>
+                </div>
+                {
+                    account ? <Avatar className='avtar2'  id="basic-button"
+                    aria-controls={open ? 'basic-menu' :  undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                    onClick={handleClick}
+                    >{account.fname[0].toUpperCase()}</Avatar>:
+                    <Avatar className='avtar'
+                    id="basic-button"
+                    aria-controls={open ? 'basic-menu' :  undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                    onClick={handleClick}
+                    >
+                        
+                    </Avatar>
+                }
+            <Menu
+            id="basic-menu"
+            anchorEl={anchorE1}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+                'aria-labelledby' :'basic-button',
+            }}
+            >
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                {  
+                account ? <MenuItem onClick={handleClose} style={{margin : 10}} onClick={logoutuser}
+                ><LogoutIcon style={{ fontSize: 16, marginRight: 3 }} />Logout</MenuItem> : ""
+                }
+            </Menu>
+            </div>
+        </nav>
+    </header>
+    
+    )
+}
+
+export default Navbaar;
---
+++ src/component/signup_sign/SignUp.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import './signup.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const SignUp = () => {
+    const [udata, setUdata] = useState({
+        fname:"",
+        email:"",
+        mobile:"",
+        password:"",
+        cpassword:""
+    });
+    console.log(udata);
+    const adddata = (e)=>{
+        const {name, value} =e.target;
+        setUdata((udata)=>{
+            return{
+                ...udata,
+                [name]:value
+            }
+        })
+
+    };
+    const senddata = async(e)=>{
+        e.preventDefault();
+        const {fname,email,mobile,password,cpassword} = udata;
+        
+        const res = await fetch("register", {
+            method:"POST",
+            headers:{
+                "Accept":"application/json",
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                fname, email, mobile, password, cpassword
+            })
+        });
+        const data = await res.json();
+        console.log(data);
+
+        if(res.status === 422 || !data){
+           // alert("no data")
+           toast.warn("invalid details",{
+            position: "top-center",
+        })
+        }else{
+            //alert("data successfully added")
+            toast.success("data successfully added",{
+                position: "top-center",
+            })
+            setUdata({...udata, fname:"",email:"",mobile:"",password:"",cpassword:""});
+        }
+    }
+return (
+    <section>
+        <div className='sign_container'>
+            <div className='sign_header'>
+                <img src='./blacklogoamazon.png' alt='amazonlogo'/>
+            </div>
+            <div className='sign_form'>
+                <form method='POST'>
+                    <h1>SignUp</h1>
+                    <div className='form_data'>
+                        <label htmlFor='fname'>Your name</label>
+                        <input type="text" onChange={adddata} value={udata.fname} name="fname" id="fname"/>
+                    </div>
+                    <div className='form_data'>
+                        <label htmlFor='email'>Email</label>
+                        <input type="text" onChange={adddata} value={udata.email} name="email" id="email"/>
+                    </div>
+                    <div className='form_data'>
+                        <label htmlFor='number'>Mobile</label>
+                        <input type="text" onChange={adddata} value={udata.mobile} name="mobile" id="mobile"/>
+                    </div>
+                    <div className='form_data'>
+                        <label htmlFor='password'>Password</label>
+                        <input type="password" onChange={adddata} value={udata.password} name="password" placeholder='At least 6 char' id="password"/>
+                    </div>
+                    <div className='form_data'>
+                        <label htmlFor='cpassword'>Password Again</label>
+                        <input type="cpassword" onChange={adddata} value={udata.cpassword} name="cpassword" id="cpassword"/>
+                    </div>
+                    <button type="submit" className='signin_btn' onClick={senddata}>Continue</button>
+                    <div className='signin_info'>
+                        <p>Already have an account? </p>
+                        <NavLink to="/login">signin</NavLink>
+                    </div> 
+                </form>
+            </div>
+            <ToastContainer/>
+        </div>
+    </section>
+  )
+}
+
+export default SignUp
